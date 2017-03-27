5a01a4a2a4861aeb5420a4f4fcfc44ec646772f8
Corey Smith <coreyas@gmail.com>
2017-03-26 23:37:17 -0400

    Refactor Carousel JSON switch into action filter attribute

--- MvcAtoZ/Controllers/CarouselController.cs
+++ MvcAtoZ/Controllers/CarouselController.cs
@@ -7,6 +7,7 @@ namespace MvcAtoZ.Controllers
 {
     public class CarouselController : BaseController
     {
+        [JsonFilter]
         public ActionResult Carousel()
         {
             var dataSource = GetDataSourceItem<Carousel>();
@@ -28,33 +29,6 @@ namespace MvcAtoZ.Controllers
             }
 
             return View(viewModel);
-        }
-
-        protected override void OnActionExecuted(ActionExecutedContext filterContext)
-        {
-            if (filterContext.HttpContext.Request.QueryString["json"] == null) return;
-
-            // Cast to ViewResultBase to support both ViewResult and PartialViewResult
-            var viewResult = filterContext.Result as ViewResultBase;
-            if (viewResult == null) return;
-            
-            filterContext.Result = new JsonResult
-            {
-                Data = viewResult.Model,
-                JsonRequestBehavior = JsonRequestBehavior.AllowGet
-            };
-        }
-
-        protected override void OnResultExecuting(ResultExecutingContext filterContext)
-        {
-            var contentType = filterContext.HttpContext.Response.ContentType;
-            filterContext.Controller.ViewBag.ContentType = contentType;
-        }
-
-        protected override void OnResultExecuted(ResultExecutedContext filterContext)
-        {
-            var contentType = filterContext.Controller.ViewBag.ContentType;
-            filterContext.HttpContext.Response.ContentType = contentType;
         }
     }
 }
---
+++ MvcAtoZ/Infrastructure/JsonFilterAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System.Web.Mvc;
+
+namespace MvcAtoZ.Infrastructure
+{
+    public class JsonFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            if (filterContext.HttpContext.Request.QueryString["json"] == null) return;
+
+            // Cast to ViewResultBase to support both ViewResult and PartialViewResult
+            var viewResult = filterContext.Result as ViewResultBase;
+            if (viewResult == null) return;
+
+            filterContext.Result = new JsonResult
+            {
+                Data = viewResult.Model,
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet
+            };
+        }
+
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            var contentType = filterContext.HttpContext.Response.ContentType;
+            filterContext.Controller.ViewBag.ContentType = contentType;
+        }
+
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            var contentType = filterContext.Controller.ViewBag.ContentType;
+            filterContext.HttpContext.Response.ContentType = contentType;
+        }
+    }
+}

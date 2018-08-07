768098bb4be6d35280b8668d4b5581557a70fea0
Corey Smith <coreyas@gmail.com>
2018-08-06 21:38:50 -0400

    Register all Glass services with service provider

--- MvcAtoZ/GlassMapperConfigurator.cs
+++ MvcAtoZ/GlassMapperConfigurator.cs
@@ -1,5 +1,7 @@
 ﻿using Glass.Mapper.Sc;
+using Glass.Mapper.Sc.Web;
 using Glass.Mapper.Sc.Web.Mvc;
+using Glass.Mapper.Sc.Web.WebForms;
 using Microsoft.Extensions.DependencyInjection;
 using Sitecore;
 using Sitecore.DependencyInjection;
@@ -11,7 +13,10 @@ namespace MvcAtoZ
         public void Configure(IServiceCollection serviceCollection)
         {
             serviceCollection.AddScoped<ISitecoreService>(sp => new SitecoreService(Context.Database));
+            serviceCollection.AddScoped<IGlassHtml>(sp => new GlassHtml(sp.GetService<ISitecoreService>()));
             serviceCollection.AddScoped<IMvcContext>(sp => new MvcContext(sp.GetService<ISitecoreService>()));
+            serviceCollection.AddScoped<IRequestContext>(sp => new RequestContext(sp.GetService<ISitecoreService>()));
+            serviceCollection.AddScoped<IWebFormsContext>(sp => new WebFormsContext(sp.GetService<ISitecoreService>()));
         }
     }
 }
